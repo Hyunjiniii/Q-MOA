b165003785b3f878d7a36dc30f530de3ede91e43
hyunjin <dksguswls121@naver.com>
2019-10-21 15:50:12 +0900

    뒤로가기 버튼 수정

--- app/src/main/java/com/example/q_moa/activity/InfoActivity.java
+++ app/src/main/java/com/example/q_moa/activity/InfoActivity.java
@@ -19,6 +19,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -45,7 +46,6 @@ import java.util.Hashtable;
 public class InfoActivity extends AppCompatActivity {
     private DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference();
     private FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-    private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private String certificate_name;
     private String uid;
@@ -68,12 +68,6 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         Intent intent = getIntent();
-        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.drawable.navbar_ic);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setDisplayShowTitleEnabled(false);
 
         //favorite setting
         viewModel = ViewModelProviders.of(this).get(FavoriteViewModel.class);
@@ -96,7 +90,6 @@ public class InfoActivity extends AppCompatActivity {
         recyclerAdapter = new TipRecyclerAdapter(items, getApplicationContext(), sub_name, series);
         recyclerView.setAdapter(recyclerAdapter);
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawerLayout);
         unfavorite_btn = (ImageView) findViewById(R.id.info_unfavorite_btn);
         favorite_btn = (ImageView) findViewById(R.id.info_favorite_btn);
         tip_null_text = (TextView) findViewById(R.id.info_tip_null_text);
@@ -111,6 +104,15 @@ public class InfoActivity extends AppCompatActivity {
         TextView agency_text = (TextView) findViewById(R.id.info_agency);
         TextView series_text = (TextView) findViewById(R.id.info_series);
         TextView category_text = (TextView) findViewById(R.id.info_category);
+
+        // 뒤로가기 버튼 클릭 이벤트
+        ImageButton back_button = (ImageButton) findViewById(R.id.info_back_btn);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         // Tip 더보기 버튼 클릭 이벤트
         TextView tip_more_button = (TextView) findViewById(R.id.info_to_tip_more);
@@ -178,8 +180,6 @@ public class InfoActivity extends AppCompatActivity {
             }
         });
 
-        onClickNavIC();
-        setHeaderView();
         setInfo("기관", agency_text);
         setInfo("계열", series_text);
         setInfo("분류", category_text);
@@ -319,78 +319,5 @@ public class InfoActivity extends AppCompatActivity {
             tip_null_text.setVisibility(View.VISIBLE);
             tip_size_text.setVisibility(View.GONE);
         }
-    }
-
-    private void onClickNavIC() {
-        navigationView = (NavigationView) findViewById(R.id.main_navigationView);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                menuItem.setChecked(true);
-                switch (menuItem.getItemId()) {
-                    case R.id.home:
-                        drawerLayout.closeDrawers();
-                        menuItem.setChecked(false);
-                        break;
-                    case R.id.favorites:
-                        drawerLayout.closeDrawers();
-                        menuItem.setChecked(false);
-                        break;
-                    case R.id.mytip:
-                        drawerLayout.closeDrawers();
-                        menuItem.setChecked(false);
-                        break;
-                    case R.id.help:
-                        drawerLayout.closeDrawers();
-                        startActivity(new Intent(InfoActivity.this, LottieActivity.class));
-                        finish();
-                        menuItem.setChecked(false);
-                        break;
-                    case R.id.logout:
-                        drawerLayout.closeDrawers();
-                        menuItem.setChecked(false);
-                        break;
-                }
-                return true;
-            }
-        });
-    }
-
-    private void setHeaderView() {
-        final View headerView = navigationView.getHeaderView(0);
-
-        // header layout 상태바 높이만큼 내려줌
-        int statusHeight = 0;
-        int resId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resId > 0) {
-            statusHeight = getResources().getDimensionPixelSize(resId);
-            LinearLayout linearLayout = (LinearLayout) headerView.findViewById(R.id.main_nav_header_linear);
-            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) linearLayout.getLayoutParams();
-            layoutParams.topMargin = statusHeight;
-            linearLayout.setLayoutParams(layoutParams);
-        }
-
-        // 프로필 이미지 라운딩
-        ImageView profileImage = (ImageView) headerView.findViewById(R.id.main_nav_header_image);
-        profileImage.setBackground(new ShapeDrawable(new OvalShape()));
-        profileImage.setClipToOutline(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    // 뒤로가기 눌렀을 때
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {  // 레이아웃 열려있으면 레이아웃 닫아줌
-            drawerLayout.closeDrawers();
-        } else
-            super.onBackPressed();
     }
 }
